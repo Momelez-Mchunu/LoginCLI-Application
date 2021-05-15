18e0f8252ef725f4974145996c9e5ce44958926f
Momelez-Mchunu <50421251+Momelez-Mchunu@users.noreply.github.com>
2021-05-15 16:30:03 +0200

    Fully working version

--- login_system.py
+++ login_system.py
@@ -1,59 +1,73 @@
-import sqlite3 , getpass
+import sqlite3 ,getpass
 
-def signup(userTableCursor): #userTableCursor is the Cursor objects for working with SQL commands(in this case)
+def signup(userTableCursor):
     """add a user to the database after certain checks have been done"""
-    print("[ SIGN UP]")
-    user_name = str(input("USERNAME >>"))  
-    while user_name and user_name.strip():  #Check if username is not empty, if not empty continue  
-        store_username = userTableCursor.execute("SELECT * FROM users WHERE usernames=?",(user_name,)).fetchall() #Check if username exists on database
-        if not store_username : # if the store_username list is empty, username has not been taken
-            password = str(getpass.getpass("PASSWORD >>"))
-            if password and password.strip():     # Try not to allow empty passwords into the table
-                userTableCursor.execute("INSERT INTO users values(?,?)",(user_name,password)) #add the user to the database
-                print("succesfully signed up..")
-                break
-            else:
-                print("invalid password,please try again.")
-                password = str(getpass.getpass("PASSWORD >>"))
-                userTableCursor.execute("INSERT INTO users values(?,?)",(user_name,password)) 
-                print("Succesfully signed up..")
-        else:
-            print("Username taken,please try again..")
-            user_name = str(input("USERNAME >>"))
-    print()
-    main() # Takes the user back to the home screen and give them the choice to terminate the program or to login/to try to sign up again if invalid username was entered
     
-
-
-def login(userTableCursor):
-    """Check user against a database and grant/deny access based on the results"""
-    print("[ LOGIN ]")
-    user_name = str(input("USERNAME >>"))
-    count = 0 
+    print("[ SIGN UP]")
+    user_name = str(input("USERNAME >> "))
     while user_name and user_name.strip():
-        if count == 0: # count being greater than 0 means user has entered incorrect username/password atleast once
-            if user_name and user_name.strip():
-                password = str(getpass.getpass("PASSWORD >>"))
-                Users = userTableCursor.execute("SELECT * FROM users WHERE usernames=? AND password=?",(user_name,password)).fetchall()
-                if Users:
-                    print("login successful!")
-                    break
+        store_username = userTableCursor.execute("SELECT * FROM users WHERE usernames=?",(user_name,)).fetchall() #Check if username exists on database
+        if not store_username:
+            password = getpass.getpass("PASSWORD >> " )
+            while password and password.strip():
+                userTableCursor.execute("INSERT INTO users values(?,?)",(user_name,password)) #add the user to the database
+                print("succesfully signed up")
+                exit(0)
+            else:
+                print("password empty, please enter a valid password")
+                password = str(getpass.getpass("PASSWORD >> ")) 
+                if password and password.strip():
+                    userTableCursor.execute("INSERT INTO users values(?,?)",(user_name,password)) 
+                    print("succesfully signed up")
+                    break   
                 else:
-                    count = count+1
-                    print("login unsuccesful,try again.")
-            
-        else:
-            user_name = str(input("USERNAME >>"))
-            if user_name and user_name.strip():
-                password = str(getpass.getpass("PASSWORD >>"))
-                Users = userTableCursor.execute("SELECT * FROM users WHERE usernames=? AND password=?",(user_name,password)).fetchall()
-                if Users:
-                    print("login successful!")
-                    break
-                else:
-                    count = count+1
-                    print("login unsuccesful,try again.")            
+                    print("Sign up unsuccesful,please start afresh")
+                    print("goodbye")
+                    exit(0)
                     
+        else:
+            print("username taken,please try again.")
+            user_name = str(input("USERNAME >> ")) 
+    else:
+        print("username empty, please enter valid username")
+    
+    
+def login(userTableCursor):
+    """Check user against a database and grant/deny access based on the results,giving 
+    the user 3 chances to enter correct password """
+    print("[ LOGIN ]")
+    user_name = str(input("USERNAME >> "))
+    number_of_trials = 0 
+    while user_name and user_name.strip():
+        if number_of_trials == 0: # number_of_trials being greater than 0 means user has entered incorrect password atleast once
+            
+            if user_name and user_name.strip(): #Check if username is not empty, if not empty continue to password
+                password = str(getpass.getpass("PASSWORD >> "))
+                users_found = userTableCursor.execute("SELECT * FROM users WHERE usernames=? AND password=?",(user_name,password)).fetchall() 
+                if users_found:
+                    print("login successful!")
+                    break
+                else:
+                    number_of_trials = number_of_trials+1
+                    print("login unsuccesful,try again.")
+        elif 0<number_of_trials<=2:
+            user_name = str(input("USERNAME >> "))
+            if user_name and user_name.strip():
+                password = str(getpass.getpass("PASSWORD >> "))
+                users_found = userTableCursor.execute("SELECT * FROM users WHERE usernames=? AND password=?",(user_name,password)).fetchall()
+                if users_found:
+                    print("login successful!")
+                    break
+                else:
+                    number_of_trials = number_of_trials+1
+                    print("login unsuccesful,try again..") 
+        else:
+            print("Please restart to attempt login,goodbye")
+            exit(0)
+    else: 
+        print("Username empty,please enter valid username")  
+    
+    
 def main():
     users = sqlite3.connect('users.db')
     database = users.cursor()
@@ -68,13 +82,11 @@ def main():
     elif user_input == '2':
         signup(database)
     else:
-        print("Invalid command")
+        print("Invalid command...")
         main()
         
     users.commit()
     users.close()
-    
-    
 if __name__=="__main__":
     main()    
 
