f845c20dfb6e16fa9a630fb38c1fe3d607b0dd06
Momelez-Mchunu <50421251+Momelez-Mchunu@users.noreply.github.com>
2021-05-26 17:59:36 +0200

    Login-CLI application with hashed password

--- login_system.py
+++ login_system.py
@@ -1,5 +1,8 @@
 import sqlite3 ,getpass
- 
+import hashlib
+import os
+from codecs import encode
+
 def signup(userTableCursor):
     """add a user to the database after certain checks have been done"""
     
@@ -10,16 +13,20 @@ def signup(userTableCursor):
         if not store_username:
             password = getpass.getpass("PASSWORD >> " )
             while password and password.strip():
-                userTableCursor.execute("INSERT INTO users values(?,?)",(user_name,password)) #add the user to the database
+                salt = os.urandom(32)    # generate a random byte string to use during hashing
+                key =  hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000) #generates a hashcode with the salt generated above                
+                userTableCursor.execute("INSERT INTO users values(?,?,?)",(user_name,key,salt)) #add the user to the database
                 print("succesfully signed up")
-                exit(0)
+                return
             else:
                 print("password empty, please enter a valid password")
                 password = str(getpass.getpass("PASSWORD >> ")) 
                 if password and password.strip():
-                    userTableCursor.execute("INSERT INTO users values(?,?)",(user_name,password)) 
+                    salt = os.urandom(32)   
+                    key =  hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)                     
+                    userTableCursor.execute("INSERT INTO users values(?,?,?)",(user_name,key,salt)) 
                     print("succesfully signed up")
-                    break   
+                    return 
                 else:
                     print("Sign up unsuccesful,please start afresh")
                     print("goodbye")
@@ -43,10 +50,17 @@ def login(userTableCursor):
             
             if user_name and user_name.strip(): #Check if username is not empty, if not empty continue to password
                 password = str(getpass.getpass("PASSWORD >> "))
-                users_found = userTableCursor.execute("SELECT * FROM users WHERE usernames=? AND password=?",(user_name,password)).fetchall() 
-                if users_found:
-                    print("login successful!")
-                    break
+                salt= userTableCursor.execute("SELECT salt FROM users WHERE usernames=?",(user_name,)).fetchall() #get salt that corresponds to entered username
+                if salt:
+                    stored_salt = salt[0]
+                    stored_salt = str(stored_salt)[3:len(str(stored_salt))-3]
+                    #print("This is stored salt 1 \n"+stored_salt)
+                    stored_salt = encode(stored_salt.encode().decode('unicode_escape'),"raw_unicode_escape")# byte encode the salt retrieved from database
+                    enter_password_hash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), stored_salt, 100000)
+                    users_found = userTableCursor.execute("SELECT * FROM users WHERE usernames=? AND key=?",(user_name,enter_password_hash)).fetchall() 
+                    if users_found:
+                        print("login successful!")
+                        break
                 else:
                     number_of_trials = number_of_trials+1
                     print("login unsuccesful,try again.")
@@ -54,10 +68,20 @@ def login(userTableCursor):
             user_name = str(input("USERNAME >> "))
             if user_name and user_name.strip():
                 password = str(getpass.getpass("PASSWORD >> "))
-                users_found = userTableCursor.execute("SELECT * FROM users WHERE usernames=? AND password=?",(user_name,password)).fetchall()
-                if users_found:
-                    print("login successful!")
-                    break
+                salt= userTableCursor.execute("SELECT salt FROM users WHERE usernames=?",(user_name,)).fetchall() #get salt from database and  
+                #print("This is the salt:\n",salt)
+                key = userTableCursor.execute("SELECT key FROM users WHERE usernames=?",(user_name,)).fetchall()
+                print(key)
+                if salt:
+                    stored_salt = salt[0]
+                    stored_salt = str(stored_salt)[3:len(str(stored_salt))-3]
+                    #print("This is stored salt 1 \n"+stored_salt)
+                    stored_salt = encode(stored_salt.encode().decode('unicode_escape'),"raw_unicode_escape")
+                    enter_password_hash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), stored_salt, 100000)
+                    users_found = userTableCursor.execute("SELECT * FROM users WHERE usernames=? AND key=?",(user_name,enter_password_hash)).fetchall()
+                    if users_found:
+                        print("login successful!")
+                        break
                 else:
                     number_of_trials = number_of_trials+1
                     print("login unsuccesful,try again..") 
@@ -90,4 +114,3 @@ def main():
 if __name__=="__main__":
     main()    
 
-    
